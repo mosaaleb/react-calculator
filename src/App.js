23fdbdda1c4169c73bd9eb41991f1599e0268753
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-11-11 22:35:25 +0200

    add Stickler and eslint config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  render = () => {
-    return (
-      <div className='App'>
-        <h1 className='App-header'>Hello From React!</h1>
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">Hello From React!</h1>
+    </div>
+  );
 }
 
 export default App;
