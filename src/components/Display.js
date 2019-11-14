ddcb0b167a1cfd62e23d24b54e34a5a60f62e43e
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-11-14 23:53:53 +0200

    display component

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Display extends React.Component {
+  render() {
+    return (
+      <div>
+        Display
+      </div>
+    );
+  }
+}
+
+export default Display;
