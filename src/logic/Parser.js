10d01fb5d7c7ae357125def0ffa7fd449cc6cd44
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-16 03:20:58 +0200

    Add infix to postfix convertion function

--- src/logic/Parser.js
+++ src/logic/Parser.js
@@ -1,7 +1,7 @@
-const Parser = () => {
-  const outPutQueue = '';
-  const operatorStack = [];
-  const operatorsPrecedence = {
+const Parser = (() => {
+  let outPutQueue = '';
+  const stack = [];
+  const precedence = {
     '÷': 3,
     '×': 3,
     '+': 2,
@@ -13,16 +13,31 @@ const Parser = () => {
 
   const cleanInfix = (infix) => {
     const withoutSpaces = infix.replace(/\s/g, '');
-    // eslint-disable-next-line no-useless-escape
-    const splitted = withoutSpaces.split(/([\+\-\×\÷])/);
+    const splitted = withoutSpaces.split(/([+\-×÷])/);
     return splitted.filter((item) => item);
   };
 
   const infixToPostfix = (infix) => {
     const infixArray = cleanInfix(infix);
+    infixArray.forEach((token) => {
+      if (isNumeric(token)) {
+        outPutQueue += `${token} `;
+      } else {
+        let last = stack[stack.length - 1];
+        while ('+-×÷'.indexOf(last) !== -1 && (precedence[token] <= precedence[last])) {
+          outPutQueue += `${stack.pop()} `;
+          last = stack[stack.length - 1];
+        }
+        stack.push(token);
+      }
+    });
+    while (stack.length > 0) {
+      outPutQueue += `${stack.pop()} `;
+    }
+    return outPutQueue;
   };
 
   return { infixToPostfix };
-};
+})();
 
 export default Parser;
